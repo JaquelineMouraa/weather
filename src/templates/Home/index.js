d7798e0de6996ff79133564501729083faa7399e
Jaqueline Moura <jaqueline.v.moura@accenture.com>
2022-02-09 16:30:30 -0300

    app routers

--- src/templates/Home/index.js
+++ src/templates/Home/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import { BiWorld } from "react-icons/bi";
 import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -14,14 +15,14 @@ export default function Home() {
 
       <div className="boxCities">
         <div className="lineCities">
-          <button>Dallol</button>
-          <button>Fairbanks</button>
-          <button>London</button>
+        <Link to="/weather" className="link">Dallol</Link>
+        <Link to="/weather" className="link">Fairbanks</Link>
+        <Link to="/weather" className="link">London</Link>
         </div>
         <div className="lineCities">
-          <button>Recife</button>
-          <button>Vancouver</button>
-          <button>Yakutsk</button>
+        <Link to="/weather" className="link">Recife</Link>
+        <Link to="/weather" className="link">Vancouver</Link>
+        <Link to="/weather" className="link">Yakutsk</Link>
         </div>
       </div>
     </div>
